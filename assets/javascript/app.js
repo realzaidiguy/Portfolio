8078415bbc2448e582a7aada9155a7bc9b8ba9db
realzaidiguy <realzaidiguy@gmail.com>
2017-04-06 13:20:24 -0400

    Initial Layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// Template Javascript Engine
+
+// begin document.ready function
+$(document).ready(function() {
+
+// ====================
+// FIRST COME VARIABLES
+// ====================
+
+
+
+// =====================
+// SECOND COME FUNCTIONS
+// =====================
+
+
+
+// ===================
+// THIRD COMES THE APP
+// ===================
+
+
+
+// end document.ready function
+});
