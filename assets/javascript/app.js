67ffa3b53ccd74c8cf480d7fe2f92aac79eeea21
realzaidiguy <realzaidiguy@gmail.com>
2017-04-06 22:18:41 -0400

    Added projects details

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,23 +3,5 @@
 // begin document.ready function
 $(document).ready(function() {
 
-// ====================
-// FIRST COME VARIABLES
-// ====================
-
-
-
-// =====================
-// SECOND COME FUNCTIONS
-// =====================
-
-
-
-// ===================
-// THIRD COMES THE APP
-// ===================
-
-
-
 // end document.ready function
 });
